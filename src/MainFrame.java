3d6b98662cfba8aab49e0ca75319d80af420c2d1
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-09-28 15:41:51 -0500

    Added MainFrame.java (still in progress)
    This is the app that opens after user has already logged in.

---
+++ src/MainFrame.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public void initialize(User user) {
+        //Panel that contains all the user details in DB
+        JPanel userPanel = new JPanel();
+        userPanel.setLayout(new GridLayout(0, 2, 5, 5));
+        userPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+        userPanel.add(new JLabel("Name"));
+        userPanel.add(new JLabel(user.name));
+        userPanel.add(new JLabel("Email"));
+        userPanel.add(new JLabel(user.email));
+        userPanel.add(new JLabel("Phone"));
+        userPanel.add(new JLabel(user.phone));
+        userPanel.add(new JLabel("Address"));
+        userPanel.add(new JLabel(user.address));
+        //Set the font for all the labels the same
+        Component[] labels = userPanel.getComponents();
+        for (int i = 0; i < labels.length; i++) {
+            labels[i].setFont(new Font("Tunga", Font.BOLD, 16));
+        }
+        add(userPanel, BorderLayout.NORTH);
+
+
+        //Set customization of the frame
+        setTitle("Calorie Tracker");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(1100, 650);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
