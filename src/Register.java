84ef981dd9779d849d4377454a03ec5b52a3b3f1
retrQJS <108301046+retrQJS@users.noreply.github.com>
2022-10-10 13:33:48 -0500

    Create Register.java

---
+++ src/Register.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+public class Register extends JFrame {
+    public void RegisterInfo(){
+        //JPanel userPanel = new JPanel();
+
+        //Set customization of the frame
+        setTitle("Registration for Calorie Tracker");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(600, 550);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
